9665642416e775d65d67f108c3cb655aff69622c
nemo13 Diu <nemo13 Diu>
2017-03-24 22:34:05 -0400

    added history files

---
+++ 20170224-example.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python;
+""" This is a test function script
+"""
+
+__author__ = "betteridiot"
+__email__ = "mdsherman@betteridiot.com"
+
+import os
+import numpy as np
+from collections import deque
+
+def foo():
+    pass
+
+
+def bar(string, num, setter):
+    """ function does something
+    
+    Args:
+        string (str): string with name
+        num (int)
+    """
+    pass
+
+
+def main(string):
+    do_something(string)
+    pass
+
+
+if __name__ = '__main__':
+    main(sys.argv[1])
